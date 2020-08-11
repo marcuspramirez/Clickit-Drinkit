80237cc979ea31f99808e584ec85ade206235426
Prabin Singh <pravin544@gmail.com>
2020-08-10 21:56:54 -0500

    Added feature to pyll image from api

--- develop/script.js
+++ develop/script.js
@@ -2,44 +2,55 @@ $(document).ready(function(){
 
     var cocktailUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
-    var userInput = "margarita";
+    // var userInput = "margarita";
 
-    $.ajax({
-        url: cocktailUrl + userInput,
-        method: "GET",
-    }).then(function(response){
-        var cocktails = response.drinks;
+    $("#searchBtn").click(function(){
+        var cocktail = $("#cocktail").val();
+        $.ajax({
+            url: cocktailUrl + "margarita",
+            method: "GET",
+        }).then(function(response) {
+            var cocktails = response.drinks;
+            var imgURL = response.strDrinkThumb;
+            var image = $("<img>").attr("src", imgURL);
 
-        for (let i = 0; i < cocktails.length; i++) {
-            console.log(cocktails[i].strDrink);
-            var cocktail = cocktails[i].strDrink;
-
-            var cocktail = $("<button>").addClass("buttonClass").attr("id", cocktails[i].strDrink).text(cocktails[i].strDrink);
-            $('#cocktailBtn').append(cocktail);
-        }
+            for (let i = 0; i < cocktails.length; i++) {
+                console.log(cocktails[i].strDrink);
+                var imgURL = cocktails[i].strDrinkThumb;
+                var image = $("<img>").attr("src", imgURL);
+                var cocktailTypes = cocktails[i].strDrink;
+                var cocktailTypes = $("<button>").addClass("buttonClass").attr("id", cocktails[i].strDrink).text(cocktails[i].strDrink);
+                $('.searchedCocktail').append(cocktailTypes,image);
+            }
     });
+});
 
     $('body').on('click', '.buttonClass', function() {
-        $('#ingredientSection').empty();
-        $('#instructionSection').empty();
+        $('.ingredientSection').empty();
+        $('.instructionSection').empty();
         var alcohol = $(this).attr('id');
+        console.log(alcohol);
+        
         $.ajax({
             url: cocktailUrl + alcohol,
             method: "GET",
         }).then(function(response){
-
-            var instruction = $("<li>").text(response.drinks[0].strInstructions);
-
+            console.log(response.drinks[0]);
+            var instruction = $("<p>").text(response.drinks[0].strInstructions);
+            
             //This loop gets ingredients and measure
             for (let i = 1; i < 16; i++) {
                 console.log(i);
-                var ingredients = $("<li>").text(response.drinks[0][`strIngredient${i}`]);
-                var measurement = $("<li>").text(response.drinks[0][`strMeasure${i}`]);
-                $('#ingredientSection').append(ingredients, measurement);
+                var ingredients = $("<p>").text(response.drinks[0][`strIngredient${i}`]);
+                var measurement = $("<p>").text(response.drinks[0][`strMeasure${i}`]);
+                $('.ingredientSection').append(ingredients);
+                $('.ingredientMesr').append(measurement);
             }
 
-            $('#instructionSection').append(instruction);
+            $('.instructionSection').append(instruction);
         });
     })
+
+    // function setVisibility(id, visibility) {
 
 });
