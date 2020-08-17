97a61fb7fa2c21615c96cb8dca1e75340b6adc33
Prabin Singh <pravin544@gmail.com>
2020-08-17 17:43:45 -0500

    Fixed page load

--- develop/script.js
+++ develop/script.js
@@ -6,31 +6,8 @@ $(document).ready(function () {
 
   var age = {};
 
-  function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-
-  if (getCookie('popupCookie') != 'submited') {
-
-    $('#ageModal').modal('show');
+  $('#ageModal').modal('show');
     initAge();
-  }
-  else{
-    $('.cocktailSlideShow').hide();
-    $("#home").show();
-    $("#ageModal").hide();
-  }
 
   // starts the age verification process
   function initAge() {
